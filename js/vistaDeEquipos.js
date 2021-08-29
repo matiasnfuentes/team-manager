6ec7bfcf2d2218038e60969a9a0a10aa77c1572e
matiasnfuentes <matiasnfuentes@gmail.com>
2021-08-29 10:15:26 -0300

    Retoque visuales a la vista de equipos

--- js/vistaDeEquipos.js
+++ js/vistaDeEquipos.js
@@ -36,15 +36,15 @@ function mostrarEquipo(equipo) {
     $("#equipos").append(`<div class="col">
                             <div class="card border-2 rounded-3">
                               <div class="card-header equipo__titulo">
-                                <h5 class="card-title col-8 mb-0 align-self-center text-start">${equipo.nombre}</h5>
-                                <button id="modify-${equipo.id}" type="button" class="col-2 btn btn-light p-1 me-1">
-                                  <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#0d6efd" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                <h5 class="card-title col-8 col-sm-10 mb-0 align-self-center text-start text-truncate">${equipo.nombre}</h5>
+                                <button id="modify-${equipo.id}" type="button" class="col-2 col-sm-1 btn btn-light p-1 me-1">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="25" fill="#0d6efd" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                                     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                                   </svg>
                                 </button>
-                                <button id="close-${equipo.id}" type="button" class="col-2 btn btn-light p-1">
-                                  <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#0d6efd" class="bi bi-x-square" viewBox="0 0 16 16">
+                                <button id="close-${equipo.id}" type="button" class="col-2 col-sm-1 btn btn-light p-1">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="25" fill="#0d6efd" class="bi bi-x-square" viewBox="0 0 16 16">
                                     <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
                                     <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"></path>
                                   </svg>
@@ -107,7 +107,7 @@ function mostrarEquipos(equipos) {
                        </form>
                     </div>
                     
-                    <div id ="equipos" class="row row-cols-1 row-cols-sm-2 row-cols-xl-3 g-4 col-12 col-lg-8 align-self-center"></div>`);
+                    <div id ="equipos" class="row row-cols-1 row-cols-sm-2 row-cols-xxl-3 g-4 col-12 col-lg-10 align-self-center"></div>`);
   
     equipos.forEach((e) => mostrarEquipo(e));
 
